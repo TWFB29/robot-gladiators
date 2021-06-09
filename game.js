d576e518c8ca0740ba2ac954d132fd4156287a60
TWFB29 <79944808+TWFB29@users.noreply.github.com>
2021-06-08 21:03:33 -0400

    Add files via upload

--- game.js
+++ game.js
@@ -9,10 +9,9 @@ var enemyHealth = 50;
 var enemyAttack = 12;
 
 var fight = function(enemyName){
-    while(enemyHealth > 0) {
+    while(playerHealth > 0 && enemyHealth > 0) {
     //window.alert("Welcome to Robot Gladiators!");
     var promptFight = window.prompt("would you like to FIGHT or SKIP this battle");
-
     // if player choses to fight, then fight
     if (promptFight === "fight" || promptFight === "FIGHT") {
     // remove enemy's health by subtracting the amount set in the playerAttack variable
@@ -27,6 +26,7 @@ var fight = function(enemyName){
     //check enemy's health
     if (enemyHealth <= 0) {
         window.alert(enemyName + " has died!");
+        break;
     }
     else {
         window.alert(enemyName + " still has " + enemyHealth + " health left.");
@@ -39,6 +39,7 @@ var fight = function(enemyName){
     //check player's health
     if (playerHealth <= 0) {
         window.alert(playerName + " has died");
+        break;
     }
     else {
         window.alert(playerName + " still has " + playerHealth + " health left.");
@@ -53,7 +54,8 @@ var fight = function(enemyName){
   if (confirmSkip) {
     window.alert(playerName + " has decided to skip this fight. Goodbye!");
     // subtract money from playerMoney for skipping
-    playerMoney = playerMoney - 2;
+    playerMoney = playerMoney - 10;
+    console.log("playerMoney", playerMoney);
   }
   // if no (false), ask question again by running fight() again
   else {
